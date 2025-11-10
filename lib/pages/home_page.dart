eee417925e5dd4c2acb560b43eca0e168e817fd5
dayeeen <nubicodes@gmail.com>
2025-11-10 21:31:20 +0700

    Add task input handling in the home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,12 +11,15 @@ class _HomePageState extends State<HomePage> {
 
   late double _deviceHeight, _deviceWidth;
   
+  String? _newTaskContent;
+
   _HomePageState();
   
   @override
   Widget build(BuildContext context) {
     _deviceHeight = MediaQuery.of(context).size.height;
     _deviceWidth = MediaQuery.of(context).size.width;
+    print("Input value is $_newTaskContent");
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: _deviceHeight * 0.15,
@@ -71,9 +74,26 @@ class _HomePageState extends State<HomePage> {
     return FloatingActionButton(
       onPressed: () {
         // Add task action
+        _displayTaskPopup();
       },
       backgroundColor: Colors.red,
       child: const Icon(Icons.add),
     );
+  }
+  void _displayTaskPopup() {
+    showDialog(context: context, builder: (BuildContext _context){
+      return AlertDialog(
+        title: const Text('Add New Task'),
+        content: TextField(
+          onSubmitted: (_value) => {},
+          onChanged:(_value) => {
+            setState(() {
+              _newTaskContent = _value;
+            })
+          },
+        ),
+        
+      );
+    });
   }
 }
